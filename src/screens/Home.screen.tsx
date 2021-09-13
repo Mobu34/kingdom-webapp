60ed63c185f0684a8b5e9640499b39d1f296e833
Mobu34 <ltidaspro@gmail.com>
2021-09-13 23:46:52 +0200

    feat(redux): added

--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState, Dispatch, useEffect, SetStateAction } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { RootState, AppDispatch } from "../store/store";
+import { itemSlice } from "../store/reducers/item.reducer";
 
 import Layout from "../components/Layout/Layout";
 
 const Home = () => {
+  const dispatch: AppDispatch = useDispatch();
+  const data = useSelector((state: RootState) => {
+    return state?.itemSlice?.items;
+  });
+
   return (
     <div>
       <Layout title="KINGDOM" />
+      <button
+        onClick={() => {
+          dispatch(itemSlice.actions.items("test_2"));
+        }}
+      >
+        CLICK
+      </button>
+      <p>{data}</p>
     </div>
   );
 };
