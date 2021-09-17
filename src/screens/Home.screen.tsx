5ccef05fea2d3b946783ce8f3d062e320ef8b16f
Mobu34 <ltidaspro@gmail.com>
2021-09-17 11:05:19 +0200

    feat(items): fetched

--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -3,8 +3,10 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { RootState, AppDispatch } from "../store/store";
 import { itemSlice } from "../store/reducers/item.reducer";
+import { apiGetItems } from "../api/get.api";
 
 import Layout from "../components/Layout/Layout";
+import Item from "../components/Item/Item";
 
 const Home = () => {
   const dispatch: AppDispatch = useDispatch();
@@ -12,17 +14,30 @@ const Home = () => {
     return state?.itemSlice?.items;
   });
 
+  useEffect(() => {
+    (async () => {
+      const response = await apiGetItems();
+      dispatch(itemSlice.actions.items(response));
+    })();
+  }, []);
+
+  console.log("data =", data);
+
   return (
     <div>
-      <Layout title="KINGDOM" />
-      <button
+      <Layout title="KINGDOM">
+        {data?.map((d) => {
+          return <Item {...d} />;
+        })}
+      </Layout>
+      {/* <button
         onClick={() => {
           dispatch(itemSlice.actions.items("test_2"));
         }}
       >
         CLICK
       </button>
-      <p>{data}</p>
+      <p>{data}</p> */}
     </div>
   );
 };
